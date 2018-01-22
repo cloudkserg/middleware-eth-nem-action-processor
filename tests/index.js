f5005c34530c793084652b80d8c12ac171720ce1
zyevegor <zyev.egor@gmail.com>
2018-01-22 14:38:27 +0300

    https://chronobank.atlassian.net/browse/MD-106 - nem-eth action processor tests + readme

--- tests/index.js
+++ tests/index.js
@@ -1,65 +1,107 @@
 require('dotenv/config');
 
 const config = require('../config'),
+  mongoose = require('mongoose'),
+  request = require('request-promise'),
+  Promise = require('bluebird'),
   expect = require('chai').expect,
   net = require('net'),
-  contract = require('truffle-contract'),
+  nem = require('nem-sdk').default,
+  amqp = require('amqplib'),
   _ = require('lodash'),
-  Web3 = require('web3'),
-  web3 = new Web3(),
-  Promise = require('bluebird'),
-  require_all = require('require-all'),
   accountModel = require('../models/accountModel'),
-  contracts = require_all({
-    dirname: _.nth(require.resolve('chronobank-smart-contracts/build/contracts/MultiEventsHistory').match(/.+(?=MultiEventsHistory)/), 0),
-    filter: /(^((ChronoBankPlatformEmitter)|(?!(Emitter)).)*)\.json$/,
-    resolve: Contract => contract(Contract)
-  }),
-  mongoose = require('mongoose');
-let accounts = [];
+  ctx = {};
 
-const TEST_RECIPIENT = 'TAX6E3CDBGEPPQNDP6AHWTDKMWPKY7PO2MK5G7QL';
-describe('core/sc processor', function () {
+mongoose.Promise = Promise; // Use custom Promises
+mongoose.connect(config.mongo.accounts.uri, {useMongoClient: true});
+
+describe('core/action processor', function () {
 
   before(async () => {
-    let provider = new Web3.providers.IpcProvider(config.web3.uri, net);
-    web3.setProvider(provider);
-    mongoose.Promise = Promise;
-    mongoose.connect(config.mongo.uri, {useMongoClient: true});
 
-    for (let contract_name in contracts) {
-      if (contracts.hasOwnProperty(contract_name)) {
-        try {
-          contracts[contract_name].setProvider(provider);
-          contracts[`${contract_name}Instance`] = await contracts[contract_name].deployed();
-        } catch (e) {
+    ctx.amqpInstance = await amqp.connect(config.rabbit.url);
+    ctx.amqpchannel = await ctx.amqpInstance.createChannel();
 
-        }
-      }
+    try {
+      await ctx.amqpchannel.assertExchange('events', 'topic', {durable: false});
+    } catch (e) {
+      ctx.amqpchannel = await ctx.amqpInstance.createChannel();
     }
+
   });
 
   after(() => {
-    web3.currentProvider.connection.end();
+    ctx.amqpInstance.close();
     return mongoose.disconnect();
   });
 
   it('add account to mongo', async () => {
-    accounts = await Promise.promisify(web3.eth.getAccounts)();
-    try {
-      await new accountModel({address: accounts[0]}).save();
-    } catch (e) {
-    }
-  });  
 
-  it('check accounts balance', async () => {
-    const erc = await contracts.ERC20Manager.deployed(),
-      token = await erc.getTokenAddressBySymbol('TIME');
-      time = await contracts.ERC20Interface.at(token),
-      balance = await time.balanceOf(accounts[0]);
+    let rBytes = nem.crypto.nacl.randomBytes(32);
+    let privateKey = nem.utils.convert.ua2hex(rBytes);
+    let keyPair = nem.crypto.keyPair.create(privateKey);
+
+    ctx.account = {
+      address: `0x${_.map(new Array(40), item=> _.random(0, 9)).join('')}`,
+      nem: nem.model.address.toAddress(keyPair.publicKey.toString(), config.nem.network)
+    };
+
+    return await new accountModel(ctx.account).save();
   });
 
-  it('test welcome bonus action', async() => {});
-  it('test time bonus action', async() => {});
-});
+  it('get initial balance of account', async () => {
 
+    let data = await request({
+      uri: `${config.nem.host}:${config.nem.port}/account/mosaic/owned?address=${ctx.account.nem}`,
+      json: true
+    });
+
+    ctx.account.mosaic = _.chain(data)
+      .get('data')
+      .find(mosaic =>
+      _.get(mosaic, 'mosaicId.namespaceId') === 'cb' &&
+      _.get(mosaic, 'mosaicId.name') === 'minutes')
+      .get('quantity', 0)
+      .value();
+
+  });
+
+  it('send deposit event', async () => {
+    ctx.amqpchannel.publish('events', `${config.rabbit.serviceName}_chrono_sc.deposit`, new Buffer(JSON.stringify({
+      name: 'Deposit',
+      payload: {
+        who: ctx.account.address,
+        amount: 100000000 * 0.017
+      }
+    })));
+  });
+
+  it('check that bonus has been sent', async () => {
+    await Promise.delay(20000);
+    let result = await accountModel.findOne({address: ctx.account.address});
+    expect(parseInt(result.maxTimeDeposit)).to.be.above(0);
+  });
+
+
+  it('check the account\'s balance', async () => {
+    await Promise.delay(60000 * 2);
+
+    let data = await request({
+      uri: `${config.nem.host}:${config.nem.port}/account/mosaic/owned?address=${ctx.account.nem}`,
+      json: true
+    });
+
+    let mosaic = _.chain(data)
+      .get('data')
+      .find(mosaic =>
+      _.get(mosaic, 'mosaicId.namespaceId') === 'cb' &&
+      _.get(mosaic, 'mosaicId.name') === 'minutes')
+      .get('quantity', 0)
+      .value();
+
+    expect(mosaic).to.be.above(ctx.account.mosaic);
+
+  });
+
+
+});
